a976cc0f66eb665babf5944692ed689c99bb9715
AshuChoudharystd <146466303+AshuChoudharystd@users.noreply.github.com>
2024-12-08 22:17:36 +0530

    7th Commit

--- application.py
+++ application.py
@@ -45,4 +45,4 @@ def predict():
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(host='0.0.0.0',debug=True,port=5000)
